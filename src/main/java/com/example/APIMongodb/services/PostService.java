b9bad4ab6bfad937872625ecafaa1297cc085b32
lucasbarsaglini <lucasbarsag@gmail.com>
2022-09-15 23:00:40 -0300

    Obtendo um post por id

---
+++ src/main/java/com/example/APIMongodb/services/PostService.java
@@ -0,0 +1,27 @@
+package com.example.APIMongodb.services;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.APIMongodb.domain.Post;
+import com.example.APIMongodb.repository.PostRepository;
+import com.example.APIMongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private PostRepository postRepository; 
+	
+	public Post findById(String id) {
+		Post user = postRepository.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+}
